01b2befe2525dd55a86eab3e7795c0cd1c3571e3
ppeak <1243320371@qq.com>
2024-02-29 22:47:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def index():
     return render_template('index.html', similarity=similarity)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=443,ssl_context=('sim.ppeak.site_bundle.crt','sim.ppeak.site.key'))
+    app.run(debug=False, host='0.0.0.0', port=443,ssl_context=('sim.ppeak.site_bundle.crt','sim.ppeak.site.key'))
