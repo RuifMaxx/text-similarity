982dde521c879179e300bbe58f7ddd0f1a4fa12d
Your Name <you@example.com>
2024-03-01 14:56:45 +0800

    m

--- app.py
+++ app.py
@@ -50,14 +50,11 @@ def calculate_similarity(text1, text2, method):
 
 @app.before_request
 def before_request():
-    if request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
+    if request.url.startswith('https://1'):
+        url = f'https://sim.ppeak.site{request.path}'
         code = 301
         return redirect(url, code=code)
-        
-    elif not request.is_secure:
-        url = request.url.replace('http://', 'https://', 1)
-        return redirect(url, code=301)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
